efc9a91efded5fe6f4dbcad2beb809b0454d428c
Emile-yu <yjg9091@gmail.com>
2020-08-26 17:35:07 +0200

    Mise en place du projet

---
+++ Prism/PrismDemo/PrismDemo/Bootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Regions;
+using Prism.Unity;
+using PrismDemo.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Unity;
+
+namespace PrismDemo
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterType(typeof(object), typeof(ViewA), "ViewA");
+            Container.RegisterType(typeof(object), typeof(ViewB), "ViewB");
+        }
+    }
+}
---
+++ Prism/PrismDemo/PrismDemo/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using PrismDemo.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PrismDemo.ViewModels
+{
+    public class ViewAViewModel : BindableBase
+    {
+        private string _firstName;
+        /// <summary>
+        /// First Name
+        /// </summary>
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { SetProperty(ref _firstName,value); }
+        }
+
+        private string _secondName;
+        /// <summary>
+        /// Second Name
+        /// </summary>
+        public string SecondName
+        {
+            get { return _secondName; }
+            set { SetProperty(ref _secondName, value);
+                //UpdateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private DateTime? _lastUpdated;
+        /// <summary>
+        /// Time
+        /// </summary>
+        public DateTime? LastUpdated
+        {
+            get { return _lastUpdated; }
+            set { SetProperty(ref _lastUpdated, value);
+                //UpdateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private IEventAggregator _eventAggregator;
+
+        public DelegateCommand UpdateCommand { get; set; }
+
+        public ViewAViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            UpdateCommand = new DelegateCommand(Execute, CanExecute).ObservesProperty(() => FirstName).ObservesProperty(() => SecondName);
+        }
+
+        private bool CanExecute()
+        {
+            return !String.IsNullOrWhiteSpace(FirstName) && !String.IsNullOrWhiteSpace(SecondName);
+        }
+
+        private void Execute()
+        {
+            LastUpdated = DateTime.Now;
+
+            _eventAggregator.GetEvent<UpdateEvent>().Publish(LastUpdated.ToString());
+        }
+    }
+}
